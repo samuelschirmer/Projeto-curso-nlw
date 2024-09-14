266363e9bf16c75741e5609dc3452d3004fc5bef
samuel <samuelschirmer190@gmail.com>
2024-09-14 15:03:14 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// arrays, objetos
 let meta = { 
     value: 'ler um livro por mês',
-    checked: false,
-    log: (info) => {
-        console.log(info)
-    } 
+    checked: true,
 }
 
-meta.value = 'não é mais ler um livro'
-meta.log(meta.value)
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
 
-// function
-const criarMeta = () => {}
+console.log(metas[1].value)
